cee8a3743c56bb9376301f1af74755b0c39ec1d2
Bindu-code <asiva325@gmail.com>
2020-04-08 16:39:30 +0530

    added maps

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit,OnInit, ElementRef, ViewChild } from '@angular/core';
 import { GitService } from '../git.service';
-import { forkJoin } from 'rxjs';
+declare var google: any;
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
- searchList;
- spinner = false;
+export class SearchComponent implements OnInit, AfterViewInit {
+  @ViewChild('pieChart',{ static: false }) pieChart: ElementRef
+  @ViewChild('map',{ static: false }) map: ElementRef
+  searchList;
+  spinner = false;
+  country = []
+  totalWorld;
+ 
   constructor(private coronaService: GitService) {
  this.search(null);
-   }
-
+    }
+    drawChart() {  
+      const data = google.visualization.arrayToDataTable(this.country);
+      const options = {
+        title: 'Covid Cases',
+        legend: {position: 'top'}
+      };
+    
+      const chart = new google.visualization.PieChart(this.pieChart.nativeElement);  
+      const chart1 = new google.visualization.GeoChart(this.map.nativeElement);
+  
+    chart.draw(data, options);
+    chart1.draw(data);
+  }
+  
+    ngAfterViewInit() {
+      google.charts.load('current', { 'packages': ['corechart']});
+    }
   search(data) {
+    this.country = []
     this.spinner = true;
-    this.coronaService.getCountries().subscribe((res) => {
+    this.coronaService.getCountries().subscribe((res: any[]) => {
+      this.totalWorld = res.shift();      ;
       this.searchList = res;
       if(data) {
         this.searchList = this.searchList.filter((res) => res.country.toLowerCase().includes(data.value.country.toLowerCase()));
       }
       this.searchList = this.searchList.sort((a,b) => b.cases - a.cases);
+      this.country.push(['Country', 'Popularity'])
+      this.searchList.forEach((element,i) => {
+        if(element.country === "USA") {          
+          this.country.push(["United States",element.cases]);
+        } else {
+          this.country.push([element.country,element.cases]);
+        }
+      });
     this.spinner = false;
+    this.drawChart();
     if(data) data.reset();
     },err => this.spinner = false);
   }
