75f7fec2aef6425e3aa82012578d31d28df3a8ed
Bindu-code <asiva325@gmail.com>
2020-04-12 10:11:01 +0530

    added maps

--- src/app/search-list/search-list.component.ts
+++ src/app/search-list/search-list.component.ts
@@ -7,10 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SearchListComponent implements OnInit {
 @Input() searchList;
+flag = {
+  1:false,
+  2:false,
+  3: false,
+  4:false,
+  5: false
+}
   ngOnInit() {
   }
 
   trackByFn(index, item) {
     return index;
+  }
+
+
+  sort(aa,bb,index) {
+    this.flag[index] = !this.flag[index];
+      this.searchList = this.flag[index] ? this.searchList.sort((a,b) => b[aa][bb] - a[aa][bb]) :
+                                           this.searchList.sort((a,b) => a[aa][bb] - b[aa][bb]);
   }
 }
